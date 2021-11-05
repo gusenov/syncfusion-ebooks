4f4b7ea11277b79d673c7df62f3f761d867e322b
Аббас Гусенов <gusenov@live.ru>
2021-11-05 19:49:58 +0600

    Обновил скрипт для загрузки списка книг и сохранения его в JSON-файл.

--- ebooks.sh
+++ ebooks.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
+AntiforgeryFieldname="YOUR_VALUE"
+cookie=".AspNetCore.Antiforgery.1ZV8OwHkBWo=YOUR_VALUE; .ASPXAUTH=YOUR_VALUE; _sid=YOUR_VALUE;"
+
 output_file="./ebooks.json"
 response=""
 
-load_more_count=12
-while true; do
-  response=$(
-    curl --silent \
-         --data-urlencode "searchWord=" \
-         --data-urlencode "TypeList=all" \
-         --data-urlencode "load-more-count=$load_more_count" \
-         --request POST "https://www.syncfusion.com/ebooks"
-  )
-
-  is_load_more_button=$(
-    echo "$response" \
-    | xmllint --html -xpath 'count(//button[@id="load-more"])' -
-  )
-
-  if [ "$is_load_more_button" -eq 0 ]; then
-    break
-  fi
-
-  load_more_count=$(( $load_more_count + 12 ))
-  echo "load_more_count=$load_more_count"
-done
+((load_more_count=2**16))
+response=$(
+  curl --silent \
+       --header "cookie: $cookie" \
+       --data-urlencode "search-word=" \
+       --data-urlencode "ebook-type-list-multi=all" \
+       --data-urlencode "AntiforgeryFieldname=$AntiforgeryFieldname" \
+       --data-urlencode "list=All" \
+       --data-urlencode "load-more-count=$load_more_count" \
+       --request POST "https://www.syncfusion.com/succinctly-free-ebooks"
+)
 
 count=$(
   echo "$response" \
@@ -53,13 +45,15 @@ for (( c=count; c>=1; c-- )); do
   )
 
   title="$(echo -e "${a_text}" | tr -d '\r\n' | sed -e 's/\xc2\xa0/ /g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-  echo "title=$title"
 
-  url="https://www.syncfusion.com$a_href"
+  url=$(echo "https://www.syncfusion.com/$a_href" | xargs)
+
   img="https:$img_src"
 
   is_book_saved=$(jq '[.ebooks[] | select(.url == "'"$url"'")] | length' "$output_file")
   if [ "$is_book_saved" -eq 0 ]; then
+    echo "- [$title]($url)"
+
     #jq '.ebooks += [{"title": "'"$title"'", "url": "'"$url"'", "img": "'"$img"'"}]' "$output_file" \
     jq '.ebooks |= [{"title": "'"$title"'", "url": "'"$url"'", "img": "'"$img"'"}] + .' "$output_file" \
     | sponge "$output_file"
